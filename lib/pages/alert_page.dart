1ea645f1df65121d99e837102be52fb37d098b7a
juan <201713632a@utea.edu.pe>
2022-11-03 21:02:59 -0500

    progreso(60%)

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
@@ -10,6 +12,77 @@ class AlertPage extends StatelessWidget {
           backgroundColor: Colors.white,
           content: Text(
               "Este es un ejemplo del AlertDialog. esto es un texto de prueba"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("Aceptar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  showMyAlert1(BuildContext context1) {
+    showDialog(
+      context: context1,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 40.0,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage("assets/images/block.png"),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text("Blog post published"),
+            ],
+          ),
+          backgroundColor: Colors.white,
+          content: Text(
+              "This blog post has been published. Team members will be able to edit this post and republish changes."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Cancelar",
+                style: TextStyle(color: Colors.purple, fontSize: 20),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "Aceptar",
+                style: TextStyle(color: Colors.purple, fontSize: 20),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  showMyAlert2(BuildContext context2) {
+    showDialog(
+      context: context2,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("AlertDialog example!!!"),
+          backgroundColor: Colors.white,
+          content: Text("hola 2"),
           actions: [
             TextButton(
               onPressed: () {
@@ -43,8 +116,20 @@ class AlertPage extends StatelessWidget {
               onPressed: () {
                 showMyAlert(context);
               },
-              child: Text("Alert"),
-            )
+              child: Text("Alert 1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert1(context);
+              },
+              child: Text("Alert 2"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert2(context);
+              },
+              child: Text("Alert 3"),
+            ),
           ],
         ),
       ),
